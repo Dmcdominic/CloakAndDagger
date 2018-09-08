6e5c9a96682522e94b81e21226b5d26f88866fe8
gantar22 <nathancarterwilliams@gmail.com>
2018-09-07 21:10:32 -0400

    init stuff

---
+++ Cloak And Dagger/Assets/Objects/scripts/event_object.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+[CreateAssetMenu(menuName = "variables/event")]
+public class event_object : ScriptableObject {
+
+	public UnityEvent e;
+
+	public void Invoke() {e.Invoke();}
+
+}
