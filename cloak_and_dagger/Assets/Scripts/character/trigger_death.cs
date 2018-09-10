8067fdc8f335224165904da22d57a068b57c7e18
Dmcdominic <dmcdominic@gmail.com>
2018-09-09 21:40:41 -0400

    Added a collision event and used it to restructure dagger collisions.

--- cloak_and_dagger/Assets/Scripts/character/trigger_death.cs
+++ cloak_and_dagger/Assets/Scripts/character/trigger_death.cs
@@ -6,16 +6,22 @@ using UnityEngine.Networking;
 public class trigger_death : NetworkBehaviour {
 
 	[SerializeField]
-	
-
+	collision_event_object trigger;
 
 	// Use this for initialization
 	void Start () {
-		
+		if (isLocalPlayer)
+			trigger.e.AddListener(on_dagger_collision);
+			
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	private void on_dagger_collision(GameObject dagger, Collision2D collision) {
+		if (collision.gameObject.Equals(this.gameObject)) {
+			die();
+		}
+	}
+
+	private void die() {
+		Destroy(this.gameObject);
 	}
 }
