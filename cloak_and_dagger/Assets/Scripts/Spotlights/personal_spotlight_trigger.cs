d9aa4c94645c7082ad8ee8fe06b4e24838d2f5b1
Dmcdominic <dmcdominic@gmail.com>
2018-09-10 16:50:06 -0400

    Created a basic Lobby UI, and added the hearbeat repeater (including personal spotlights).

---
+++ cloak_and_dagger/Assets/Scripts/Spotlights/personal_spotlight_trigger.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class personal_spotlight_trigger : MonoBehaviour {
+
+	[SerializeField]
+	event_object global_heartbeat_trigger;
+
+	[SerializeField]
+	event_object global_spotlight_off_trigger;
+
+	[SerializeField]
+	event_object global_spotlight_on_trigger;
+
+	private Animator animator;
+
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+
+		if (global_heartbeat_trigger) {
+			global_heartbeat_trigger.e.AddListener(heartbeat);
+		}
+		if (global_spotlight_off_trigger) {
+			global_spotlight_off_trigger.e.AddListener(off);
+		}
+		if (global_spotlight_on_trigger) {
+			global_spotlight_on_trigger.e.AddListener(on);
+		}
+	}
+	
+	private void heartbeat() {
+		animator.Play("Heartbeat");
+	}
+
+	private void on() {
+		animator.Play("On");
+	}
+
+	private void off() {
+		animator.Play("Off");
+	}
+
+}
---
+++ cloak_and_dagger/Assets/Scripts/event_repeater.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class event_repeater : NetworkBehaviour {
+
+	[SerializeField]
+	private bool serverOnly;
+
+	[SerializeField]
+	private bool autostart;
+
+	[SerializeField]
+	private float interval;
+
+	[SerializeField]
+	private event_object event_to_trigger;
+
+
+	// Use this for initialization
+	void Start() {
+		if (autostart) {
+			start_repeater();
+		}
+	}
+
+	public void start_repeater() {
+		if (serverOnly && !isServer) {
+			return;
+		}
+		StartCoroutine(triggerEvent());
+	}
+
+	public void stop_repeater() {
+		StopAllCoroutines();
+	}
+
+	IEnumerator triggerEvent() {
+		while (true) {
+			yield return new WaitForSeconds(interval);
+			Rpc_trigger_event();
+		}
+	}
+
+	// Triggers the event on all clients
+	[ClientRpc]
+	private void Rpc_trigger_event() {
+		event_to_trigger.Invoke();
+	}
+
+}
