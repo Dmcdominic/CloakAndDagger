25561ada4e2a584c6f044bb2fefb60747b13953b
Dmcdominic <dmcdominic@gmail.com>
2018-10-07 18:52:19 -0400

    Working on the win_condition_controller and the first WC controller to inherit it: kill count.

---
+++ cloak_and_dagger/Assets/Scripts/game_control/WC_kill_count_controller.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WC_kill_count_controller : win_condition_controller {
+	public override win_condition win_Condition {
+		get { return win_condition.kill_count; }
+	}
+
+	protected Dictionary<byte, player_kill_count_stats> player_stats_dict;
+	protected Dictionary<byte, team_kill_count_stats> team_stats_dict;
+	protected override player_stats get_player_stats(byte playerID) {
+		return player_stats_dict[playerID];
+	}
+	protected override team_stats get_team_stats(byte teamID) {
+		return team_stats_dict[teamID];
+	}
+
+
+	// This is called right when the scene is loaded, after the base Awake.
+	// Use this for initialization instead of Awake.
+	protected override void init() {
+		// TODO - intialize the player_stats_dict and team_stats_dict
+		// based on info passed here from the lobby or host or whatever
+	}
+
+	// This is called when the game is started
+	protected override void on_game_start() {
+	}
+
+	// This is called when a player is killed,
+	// AFTER the stats dicts are updated with kill & death counters
+	protected override void on_player_killed() {
+		foreach (team_kill_count_stats team in team_stats_dict.Values) {
+			if (team.kill_count > win_Con_Config.int_options[winCon_int_option.kill_limit]) {
+				end_game();
+			}
+		}
+	}
+
+	// Put in anything that you need to do right before ending the game
+	// that is specific to this win_con
+	private void end_game() {
+		end_game_general(false);
+	}
+
+}
+
+
+// These are player stat and team stat classes for you to store additional win condition info
+public class player_kill_count_stats : player_stats {
+}
+
+public class team_kill_count_stats : team_stats {
+}
--- cloak_and_dagger/Assets/Scripts/game_control/win_condition_controller.cs
+++ cloak_and_dagger/Assets/Scripts/game_control/win_condition_controller.cs
@@ -9,19 +9,19 @@ public abstract class win_condition_controller : MonoBehaviour {
 
 	// Events to listen to
 	public event_object game_start_trigger;
-	public int_event_object player_killed; // TODO - should this be some kind of kill_data struct, that includes HOW they died, WHO killed them, etc.
+	public event_object player_killed; // TODO - This should be the death_event that we made on Ning's branch
 
 	// Events to trigger
 	public event_object trigger_on_game_over; // TODO - this needs to store some kind of game_over_data
 
 	// Game variables
-	private float timer = 0;
+	public bool_var ingame_state;
+
+	public float_var game_timer;
 	private float time_limit;
 
-	// TODO - should these player stat dictionaries be combined into one, that just stores some kind of player_stats struct for each player?
-	// Should the player objects just store them themselves? This would make leaderboards and stuff really trick though...
-	private Dictionary<int, int> player_kill_count; // TODO - Should the keys be ints, for player ID's? Or is there an abstracted datatype for player ID?
-	private Dictionary<int, int> player_death_count;
+	protected abstract player_stats get_player_stats(byte playerID);
+	protected abstract team_stats get_team_stats(byte teamID);
 
 
 	// Initialization
@@ -30,22 +30,71 @@ public abstract class win_condition_controller : MonoBehaviour {
 			gameObject.SetActive(false);
 			return;
 		}
-
-		time_limit = win_Con_Config.float_options[winCon_float_option.time_limit];
-
-		if (game_start_trigger) {
-			game_start_trigger.e.AddListener(on_game_start);
+		if (player_killed) {
+			player_killed.e.AddListener(on_player_killed);
 		}
+		if (game_start_trigger) {
+			game_start_trigger.e.AddListener(on_game_start_general);
+		}
+
+		ingame_state.val = false;
+		game_timer.val = 0;
+		time_limit = win_Con_Config.float_options[winCon_float_option.time_limit];
+		init();
 	}
 	
 	protected void Update () {
-		timer += Time.deltaTime;
-		if (time_limit != 0 && time_limit < timer) {
-			end_game();
+		game_timer.val += Time.deltaTime;
+		if (time_limit != 0 && time_limit < game_timer.val) {
+			end_game_general(true);
 		}
 	}
 
-	protected abstract void on_game_start();
-	protected abstract void end_game(); // TODO - needs to account for time limit end, and other versions of game ending?
+	// Is called by the game_start_trigger event
+	private void on_game_start_general() {
+		ingame_state.val = true;
+		on_game_start();
+	}
 
+	// Is called by the player_killed event
+	// TODO - must take in the actual death_event struct
+	private void on_player_killed_general() {
+		byte killeD_placeholder = 1;
+		player_stats killed = get_player_stats(killeD_placeholder);
+		killed.death_count++;
+		get_team_stats(killed.teamID).death_count++;
+		if (true) { // TODO - replace this with condition if there is a killer or not
+			byte killeR_placeholder = 2;
+			player_stats killer = get_player_stats(killeR_placeholder);
+			killer.kill_count++;
+		}
+		on_player_killed();
+	}
+
+	// Should be called when you want to end the game (i.e. when the win con is met or time runs out)
+	protected void end_game_general(bool timeout) {
+		ingame_state.val = false;
+		// TODO - some other stuff here, like the leaderboard
+	}
+
+	protected abstract void init(); // Initialize your controller values using the win_Con_Config
+	protected abstract void on_game_start(); // Start the game
+	protected abstract void on_player_killed(); // Called when a player_death_event is received, after general_stats is updated
+	
+}
+
+
+// Stores a single player's stats for this match
+public class player_stats {
+	public byte playerID;
+	public byte teamID;
+	public int kill_count;
+	public int death_count;
+}
+
+// Stores a single team's stats for this match
+public class team_stats {
+	public byte teamID;
+	public byte kill_count;
+	public byte death_count;
 }
