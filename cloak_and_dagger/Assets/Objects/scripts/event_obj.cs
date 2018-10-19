3cba866a89d92f23e70799762fa8b51b98877b65
gantar22 <nathancarterwilliams@gmail.com>
2018-10-19 10:22:41 -0400

    fixed editor

--- cloak_and_dagger/Assets/Objects/scripts/event_obj.cs
+++ cloak_and_dagger/Assets/Objects/scripts/event_obj.cs
@@ -16,22 +16,6 @@ public class event_object : ScriptableObject
 
 
 
-[CustomEditor(typeof(event_object))]
-public class event_object_drawer : Editor
-{
-    public override void OnInspectorGUI()
-    {
-        DrawDefaultInspector();
-
-        event_object o = (event_object)target;
-        if (GUILayout.Button("Push Me!"))
-        {
-            o.Invoke();
-        }
-    }
-}
-
-
 [CreateAssetMenu(menuName = "variables/generic event")]
 public class gen_event<T> : ScriptableObject
 {
---
+++ cloak_and_dagger/Assets/Scripts/Editor/buttons!.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(event_object))]
+public class event_object_drawer : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        event_object o = (event_object)target;
+        if (GUILayout.Button("Push Me!"))
+        {
+            o.Invoke();
+        }
+    }
+}
+
