70a8fbd3ffba81e8a555334a37fe32dfa232fd70
jymmot <tommy.vvms@gmail.com>
2018-10-21 13:19:29 -0400

    Duplicated the dungeon map, and made prefabs for 3D walls

---
+++ cloak_and_dagger/Assets/Scripts/Spotlights/movement_2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement_2 : MonoBehaviour {
+
+	private Vector3 startposition;
+	private float xstart;
+	public bool left;
+	public float speed;
+	private float l;
+
+	// Use this for initialization
+	private void Start () {
+		l = left ? -1.0f : 1.0f;
+		xstart = -30.0f * l;
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+		transform.position = new Vector3(l * ((Time.time*speed)%90) + xstart, transform.position.y, transform.position.z);
+	}
+}
