9f028b5b026ae606192802466f0ed5f68d2a441b
gantar22 <nathancarterwilliams@gmail.com>
2018-11-01 22:14:19 -0400

    system

--- cloak_and_dagger/Assets/Scripts/dagger/destroy_dagger.cs
+++ cloak_and_dagger/Assets/Scripts/dagger/destroy_dagger.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[SerializeField]
+using System;
+
+[Serializable]
 public struct unit { }
 
 public class destroy_dagger : sync_behaviour<unit> {
