e6bcf8861537e8bf399c4894cb5ece0fd591a721
LAPTOP-US6S8JCB\Cn's Thinkpad X1C <ningc@andrew.cmu.edu>
2018-11-04 16:32:35 -0500

    Baslc UI for radial cooldown.

---
+++ cloak_and_dagger/Assets/Scripts/game_control/ui/cooldown_indicator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class cooldown_indicator : MonoBehaviour {
+
+    public Image timerMask;
+
+    public bool active;
+    public float cooldown;
+    public float timeLeft;
+
+    // initialize
+    void Start() {
+        timerMask.fillAmount = 0;
+    }
+
+    // run the cooldown
+    void Update () {
+        if (active) {
+            timeLeft -= Time.deltaTime;
+            if (timeLeft < 0)
+                timeLeft = 0;
+            updateUI();
+            completeAction();
+        }
+	}
+
+    // activate the cooldown
+    public void activateCooldown() {
+        active = true;
+    }
+
+    void updateUI() {
+        timerMask.fillAmount = timeLeft / cooldown;
+    }
+
+    // stop the cooldown indicator
+    public void completeAction() {
+        if (timeLeft > 0)
+            return;
+        active = false;
+        timeLeft = cooldown;
+    }
+}
