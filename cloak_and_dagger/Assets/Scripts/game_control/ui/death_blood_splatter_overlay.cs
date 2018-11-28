b507b74da3a0137ebf51a01083f7a3b945b490cc
Dmcdominic <dmcdominic@gmail.com>
2018-11-28 14:58:38 -0500

    Added death blood splatter overlay.

---
+++ cloak_and_dagger/Assets/Scripts/game_control/ui/death_blood_splatter_overlay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class death_blood_splatter_overlay : MonoBehaviour {
+
+	public int_event_object pre_local_death;
+	public int_var local_id;
+
+	private Animator animator;
+
+
+	private void Awake() {
+		if (pre_local_death) {
+			pre_local_death.e.AddListener(on_local_death);
+		}
+		animator = GetComponent<Animator>();
+	}
+
+	private void on_local_death(int placeholder) {
+		animator.SetTrigger("splatter");
+	}
+
+}
