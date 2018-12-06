1ee1c4a408018eec34ee5255130d54f7e017366c
gantar22 <nathancarterwilliams@gmail.com>
2018-12-05 22:05:28 -0500

    everything but a little bit of text quality

---
+++ cloak_and_dagger/Assets/add_hoc_animator_shit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class add_hoc_animator_shit : MonoBehaviour {
+
+    Animator am;
+
+    [SerializeField]
+    event_object char_out;
+
+	// Use this for initialization
+	void Start () {
+        am = GetComponent<Animator>();
+        char_out.e.AddListener(() => { if (am.GetCurrentAnimatorStateInfo(0).IsName("characters_in") ) am.SetTrigger("toggle"); });
+	}
+	
+	
+}
